d9d77bd330468106889f57b4f078b6d34e391218
CJ Burkey <cjburkey01@gmail.com>
2018-12-02 15:50:47 -0500

    Add new tests

--- src/test/java/com/cjburkey/bullet/classDec/TestAClassDec.java
+++ src/test/java/com/cjburkey/bullet/classDec/TestAClassDec.java
@@ -22,8 +22,66 @@ public class TestAClassDec {
         assertEquals(program.programIn.get().classes.size(), 1);
         assertEquals(program.programIn.get().classes.get(0).identifier, identifier);
         assertFalse(program.programIn.get().classes.get(0).types.isPresent());
-        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.size(), 0);
         assertEquals(program.programIn.get().classes.get(0).classMembers.variableDecs.size(), 0);
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.size(), 0);
+    }
+    
+    @Test
+    public void testConstructorInClass() {
+        String identifier = "ExampleClass";
+        String source = "class " + identifier + " { def _() {  } }";
+        
+        AProgram program = TestHelper.getProgram(source);
+        assert(program.programIn.isPresent());
+        assertEquals(program.programIn.get().classes.size(), 1);
+        assertEquals(program.programIn.get().classes.get(0).identifier, identifier);
+        assertFalse(program.programIn.get().classes.get(0).types.isPresent());
+        assertEquals(program.programIn.get().classes.get(0).classMembers.variableDecs.size(), 0);
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.size(), 1);
+        assert(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).isConstructor);
+        assertFalse(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).arguments.isPresent());
+    }
+    
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    @Test
+    public void testMethodInClass() {
+        String identifier1 = "ExampleClass";
+        String identifier2 = "exampleMethod";
+        String source = "class " + identifier1 + " { def " + identifier2 + "() { } }";
+        
+        AProgram program = TestHelper.getProgram(source);
+        assert(program.programIn.isPresent());
+        assertEquals(program.programIn.get().classes.size(), 1);
+        assertEquals(program.programIn.get().classes.get(0).identifier, identifier1);
+        assertFalse(program.programIn.get().classes.get(0).types.isPresent());
+        assertEquals(program.programIn.get().classes.get(0).classMembers.variableDecs.size(), 0);
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.size(), 1);
+        assert(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).identifier.isPresent());
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).identifier.get(), identifier2);
+        assertFalse(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).arguments.isPresent());
+    }
+    
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    @Test
+    public void testMethodsInClass() {
+        String identifier1 = "ExampleClass";
+        String identifier2 = "exampleMethod1";
+        String identifier3 = "exampleMethod2";
+        String source = "class " + identifier1 + " { def " + identifier2 + "() { } def " + identifier3 + "() { } }";
+        
+        AProgram program = TestHelper.getProgram(source);
+        assert(program.programIn.isPresent());
+        assertEquals(program.programIn.get().classes.size(), 1);
+        assertEquals(program.programIn.get().classes.get(0).identifier, identifier1);
+        assertFalse(program.programIn.get().classes.get(0).types.isPresent());
+        assertEquals(program.programIn.get().classes.get(0).classMembers.variableDecs.size(), 0);
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.size(), 2);
+        assert(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).identifier.isPresent());
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).identifier.get(), identifier2);
+        assertFalse(program.programIn.get().classes.get(0).classMembers.functionDecs.get(0).arguments.isPresent());
+        assert(program.programIn.get().classes.get(0).classMembers.functionDecs.get(1).identifier.isPresent());
+        assertEquals(program.programIn.get().classes.get(0).classMembers.functionDecs.get(1).identifier.get(), identifier3);
+        assertFalse(program.programIn.get().classes.get(0).classMembers.functionDecs.get(1).arguments.isPresent());
     }
     
 }
